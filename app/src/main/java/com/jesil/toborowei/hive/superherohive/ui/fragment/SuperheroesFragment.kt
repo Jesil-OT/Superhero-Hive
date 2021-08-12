da66a851dbbb6e9253e8265f4e143a8587853665
Jesil OT <toboroweijesil@gmail.com>
2021-08-12 16:35:54 -0700

    added error screen

--- app/src/main/java/com/jesil/toborowei/hive/superherohive/ui/fragment/SuperheroesFragment.kt
+++ app/src/main/java/com/jesil/toborowei/hive/superherohive/ui/fragment/SuperheroesFragment.kt
@@ -1,5 +1,6 @@
 package com.jesil.toborowei.hive.superherohive.ui.fragment
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -10,6 +11,9 @@ import com.jesil.toborowei.hive.superherohive.databinding.FragmentSuperheroesBin
 import com.jesil.toborowei.hive.superherohive.model.viewmodel.SuperheroesViewModel
 import com.jesil.toborowei.hive.superherohive.utils.DataResult
 import com.jesil.toborowei.hive.superherohive.utils.adapter.SuperheroesAdapter
+import com.jesil.toborowei.hive.superherohive.utils.hideShimmerUtils
+import com.jesil.toborowei.hive.superherohive.utils.showLottieUtil
+import com.jesil.toborowei.hive.superherohive.utils.showShimmerUtils
 import dagger.hilt.android.AndroidEntryPoint
 
 /**
@@ -19,36 +23,56 @@ import dagger.hilt.android.AndroidEntryPoint
  */
 @AndroidEntryPoint
 class SuperheroesFragment : Fragment(R.layout.fragment_superheroes) {
-    private val viewModel : SuperheroesViewModel by viewModels()
-    private var _binding : FragmentSuperheroesBinding? = null
+    private val viewModel: SuperheroesViewModel by viewModels()
+    private var _binding: FragmentSuperheroesBinding? = null
     private val binding get() = _binding!!
     private val superheroesAdapter: SuperheroesAdapter by lazy {
         SuperheroesAdapter(null)
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _binding = FragmentSuperheroesBinding.bind(view)
-        Log.d("TAG", "onViewCreated: ")
         initSuperHeroData()
-        binding.apply {
-
-        }
     }
 
+    @SuppressLint("SetTextI18n")
     private fun initSuperHeroData() {
-        viewModel.heroDataList.observe(viewLifecycleOwner){ result ->
-            when(result){
-               is DataResult.Success -> {
-                   Log.d("TAG", "initSuperHeroData: ${result.data}")
-                   superheroesAdapter.submitList(result.data)
-                   binding.superheroRecyclerView.adapter = superheroesAdapter
-               }
+        viewModel.heroDataList.observe(viewLifecycleOwner) { result ->
+            when (result) {
+                is DataResult.Success -> {
+                    superheroesAdapter.submitList(result.data)
+                    binding.apply {
+                        superheroRecyclerView.apply {
+                            showShimmerUtils()
+                            superheroRecyclerView.adapter = superheroesAdapter
+                        }
+                        shimmerViewContainer.apply {
+                            hideShimmerUtils()
+                            stopShimmer()
+                        }
+                    }
+                }
                 is DataResult.Error -> {
-                    Log.d("TAG", "initSuperHeroData: ${result.error}")
+                    binding.apply{
+                        lottieAnimationViewNoInternet.showLottieUtil()
+                        textViewError.text = "${result.error.message}."
+                        shimmerViewContainer.apply {
+                            hideShimmerUtils()
+                            stopShimmer()
+                        }
+                    }
                 }
                 is DataResult.Loading -> {
-                    Log.d("TAG", "initSuperHeroData: Loading....")
+                    binding.apply {
+                        superheroRecyclerView.hideShimmerUtils()
+                        shimmerViewContainer.apply {
+                            showShimmerUtils()
+                            startShimmer()
+                        }
+
+                    }
                 }
             }
         }
