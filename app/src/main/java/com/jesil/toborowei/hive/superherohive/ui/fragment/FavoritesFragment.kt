92282368300d32aa532e2cd293baa716088d6e56
Jesil OT <toboroweijesil@gmail.com>
2021-08-15 14:27:51 -1100

    not done yet... about to add percentage

--- app/src/main/java/com/jesil/toborowei/hive/superherohive/ui/fragment/FavoritesFragment.kt
+++ app/src/main/java/com/jesil/toborowei/hive/superherohive/ui/fragment/FavoritesFragment.kt
@@ -1,11 +1,13 @@
 package com.jesil.toborowei.hive.superherohive.ui.fragment
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.jesil.toborowei.hive.superherohive.R
+import com.jesil.toborowei.hive.superherohive.databinding.FragmentFavoritesBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -14,6 +16,21 @@ import com.jesil.toborowei.hive.superherohive.R
  */
 
 class FavoritesFragment : Fragment(R.layout.fragment_favorites) {
+    private var _binding: FragmentFavoritesBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentFavoritesBinding.bind(view)
+        binding.apply {
+
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 
 
 }
